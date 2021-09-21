83231a1a57ff1cd4bdb7015a7af47f8a2d7a3883
Gaston Farias <gastonfariasgarzon@hotmail.com>
2021-09-20 21:50:53 -0300

    CREACION BASE DE DATOS

---
+++ app/src/main/java/com/example/recorriendolahistoria/entidades/Preguntas.java
@@ -0,0 +1,101 @@
+package com.example.recorriendolahistoria.entidades;
+
+public class Preguntas {
+
+    private Integer id;
+    private String Pregunta;
+    private String RespCorrecta;
+    private String RespIncorrecta1;
+    private String RespIncorrecta2;
+    private String RespIncorrecta3;
+    private Integer TipoPregunta;
+    private Integer Guia;
+    private Integer Puntos;
+
+    public Preguntas(Integer id, String pregunta, String respCorrecta, String respIncorrecta1,
+                     String respIncorrecta2, String respIncorrecta3, Integer tipoPregunta,
+                     Integer guia, Integer puntos) {
+
+        this.id = id;
+        Pregunta = pregunta;
+        RespCorrecta = respCorrecta;
+        RespIncorrecta1 = respIncorrecta1;
+        RespIncorrecta2 = respIncorrecta2;
+        RespIncorrecta3 = respIncorrecta3;
+        TipoPregunta = tipoPregunta;
+        Guia = guia;
+        Puntos = puntos;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPregunta() {
+        return Pregunta;
+    }
+
+    public void setPregunta(String pregunta) {
+        Pregunta = pregunta;
+    }
+
+    public String getRespCorrecta() {
+        return RespCorrecta;
+    }
+
+    public void setRespCorrecta(String respCorrecta) {
+        RespCorrecta = respCorrecta;
+    }
+
+    public String getRespIncorrecta1() {
+        return RespIncorrecta1;
+    }
+
+    public void setRespIncorrecta1(String respIncorrecta1) {
+        RespIncorrecta1 = respIncorrecta1;
+    }
+
+    public String getRespIncorrecta2() {
+        return RespIncorrecta2;
+    }
+
+    public void setRespIncorrecta2(String respIncorrecta2) {
+        RespIncorrecta2 = respIncorrecta2;
+    }
+
+    public String getRespIncorrecta3() {
+        return RespIncorrecta3;
+    }
+
+    public void setRespIncorrecta3(String respIncorrecta3) {
+        RespIncorrecta3 = respIncorrecta3;
+    }
+
+    public Integer getTipoPregunta() {
+        return TipoPregunta;
+    }
+
+    public void setTipoPregunta(Integer tipoPregunta) {
+        TipoPregunta = tipoPregunta;
+    }
+
+    public Integer getGuia() {
+        return Guia;
+    }
+
+    public void setGuia(Integer guia) {
+        Guia = guia;
+    }
+
+    public Integer getPuntos() {
+        return Puntos;
+    }
+
+    public void setPuntos(Integer puntos) {
+        Puntos = puntos;
+    }
+}
---
+++ app/src/main/java/com/example/recorriendolahistoria/utilidades/Utilidades.java
@@ -0,0 +1,25 @@
+package com.example.recorriendolahistoria.utilidades;
+
+public class Utilidades {
+
+    //Constantes campos de tabla PREGUNTAS
+    public static final String TABLA_PREGUNTAS = "preguntas";
+    public static final String CAMPO_ID = "id";
+    public static final String CAMPO_PREGUNTA = "Pregunta";
+    public static final String CAMPO_RESP_CORRECTA = "RespCorrecta";
+    public static final String CAMPO_RESP_INCORRECTA1 = "RespIncorrecta1";
+    public static final String CAMPO_RESP_INCORRECTA2 = "RespIncorrecta2";
+    public static final String CAMPO_RESP_INCORRECTA3 = "RespIncorrecta3";
+    public static final String CAMPO_TIPO_PREGUNTA = "TipoPregunta";
+    public static final String CAMPO_GUIA = "Guia";
+    public static final String CAMPO_PUNTOS = "Puntos";
+
+
+    public static final String CREAR_TABLA_PREGUNTAS = "CREATE TABLE "+TABLA_PREGUNTAS+" " +
+            "("+CAMPO_ID+" INTEGER, "+CAMPO_PREGUNTA+" TEXT, "+CAMPO_RESP_CORRECTA+" TEXT, "+CAMPO_RESP_INCORRECTA1+ " TEXT, "
+            +CAMPO_RESP_INCORRECTA2+" TEXT, "+CAMPO_RESP_INCORRECTA3+" TEXT, "+CAMPO_TIPO_PREGUNTA+
+            " INTEGER, "+CAMPO_GUIA+" INTEGER, "+CAMPO_PUNTOS+" INTEGER)";
+
+
+
+}
